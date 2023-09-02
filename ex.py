17a6ee1c32e847ad09871884707bfb78f44b3a18
dias7367 <143860024+dias7367@users.noreply.github.com>
2023-09-02 11:00:12 +0700

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,36 @@
+#variabel dan tipe data
+
+x = 5 #integer
+y = 10
+z = "saya" #string
+a = "45"
+#float
+b = 3.14
+#Boolean True or False
+
+#Aritmatika
+
+c = x + y 
+print("hasil penjumlahan",c)
+
+#penjumlahan +
+#pengurangan -
+#perkalian *
+#pembagian /
+#pangkat **
+#pembagian //
+#modulus %
+
+f = 7
+g = 3
+h = 6
+
+i = f * g 
+print ("hasil perkalian",i)
+j = f * h / g 
+print (j) 
+#persegi_panjang
+
+
+nama = int(input("masukkan nama"))
+print(nama)
